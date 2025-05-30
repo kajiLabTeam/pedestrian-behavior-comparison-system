bbb61481512bdc92c5001fc90466020c98755e99
MihoMiho14134 <153794136+MihoMiho14134@users.noreply.github.com>
2025-05-30 17:50:57 +0900

    fix:V-tグラフ

--- calculation_walkingSpeed.py
+++ calculation_walkingSpeed.py
@@ -8,14 +8,19 @@ import japanize_matplotlib
 # CSVファイルを読み込み
 df = pd.read_csv("0527walking_trajectory.csv")
 
+# # 必要な列があるか確認
+# if not set(['x', 'y', 'time']).issubset(df.columns):
+#     raise ValueError("CSVに'x', 'y', 'time'列が必要です")
+
 # 必要な列があるか確認
-if not set(['x', 'y', 'time']).issubset(df.columns):
-    raise ValueError("CSVに'x', 'y', 'time'列が必要です")
+if not set(['time','x', 'y',]).issubset(df.columns):
+    raise ValueError("CSVに 'time','x', 'y',列が必要です")
 
 # 座標差分と時間差分を計算
+dt = df['time'].diff()
 dx = df['x'].diff()
 dy = df['y'].diff()
-dt = df['time'].diff()
+
 
 # 移動距離と速度を計算（速度 = 距離 / 時間）
 distance = np.sqrt(dx**2 + dy**2)
